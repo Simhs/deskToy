8007e4c4ad0ca05cc15ae02bd766f9eda17d6392
Simhs <shs900701@gmail.com>
2017-06-25 00:39:52 +0900

    Update serv.py

--- lib/Server/serv.py
+++ lib/Server/serv.py
@@ -6,7 +6,7 @@ class UDPserver:
     def __init__(self,machine):
         self.svrsock = socket(AF_INET, SOCK_DGRAM)
         self.svrsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.svrsock.bind(('', 5001))  # 로컬호스트에 5001포트로 바인딩
+        self.svrsock.bind(('', 25031))  # 로컬호스트에 5001포트로 바인딩
         self.machine = machine
 
     def recv(self):
